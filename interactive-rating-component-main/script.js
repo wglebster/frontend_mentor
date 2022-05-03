2e1462208caf1637867f0175007381e9ad2e22f9
Gleb V <wglebster@gmail.com>
2022-05-03 19:29:24 +0100

    update interactive rating component

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,7 +1,25 @@
-var rating = "";
-function getRating(event) {
-    rating = event.currentTarget;
-    
+let rating = document.getElementsByClassName("rating")[i];
 
-alert(rating)
+function changeColor(){
+    switch(rating) {
+        case 0: 
+        rating[0].style.backgroundColor = "#fb7413";
+        break;
+        case 1:
+        rating[1].style.backgroundColor = "#fb7413";
+        break;
+        case 2:
+        rating[2].style.backgroundColor = "#fb7413";
+        break;
+        case 3: 
+        rating[3].style.backgroundColor = "#fb7413";
+        break;
+        case 4:
+        rating[4].style.backgroundColor = "#fb7413";
+        break;
+    }
+
+
+
+    rating[i].style.backgroundColor = "#fb7413";
 }

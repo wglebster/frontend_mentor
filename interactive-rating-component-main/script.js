a910c307316c7a215c045745ecd0ed6a2df8eef6
Gleb V <wglebster@gmail.com>
2022-05-04 22:07:39 +0100

    update js code

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -21,3 +21,18 @@ let ratings = document.getElementsByTagName("li");
 for (let i = 0; i < ratings.length; i++) {
     ratings[i].style.backgroundColor = changeColor;
 }; */
+const ids = ["rate1", "rate2", "rate3", "rate4", "rate5"];
+const ratings = [];
+ids.forEach(function(id){
+    ratings.push(document.getElementById(id));
+});
+
+const changeColor = (e) => {
+    e.target.style.backgroundColor = "fb7413";
+};
+
+let eventAssignment = (rating) => {
+    rating.onclick = function(){
+        changeColor(e);
+    }
+};
